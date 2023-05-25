f7fdf2005b29e150a7620b730fcfe167632fa31d
Simone Carcagnì <simonecarcagni@libero.it>
2023-05-25 17:27:39 +0200

    Aggiunta rotta di techlogies.index

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\Admin\TypeController;
+use App\Http\Controllers\Admin\TechnologyController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -34,6 +35,9 @@ Route::middleware(['auth', 'verified'])
         ]);
         Route::resource('types', TypeController::class)->parameters([
             'types' => 'type:slug'
+        ])->only(['index']);
+        Route::resource('technologies', TechnologyController::class)->parameters([
+            'technologies' => 'technologies:slug'
         ])->only(['index']);
     });
 
